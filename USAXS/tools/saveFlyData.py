3629e7de00269c708df4b8499df06b072a80ab42
jemian <jemian@3cb32c87-4c34-0410-8c40-b60f3c13e47e>
2013-12-05 22:16:30 +0000

    big changes: clean-up registry of groups and PVs with links to parents and children, no need to search for this now, also devised mechanism to limit recorded length of certain array PVs based on the value of another PV.

--- USAXS/tools/saveFlyData.py
+++ USAXS/tools/saveFlyData.py
@@ -19,42 +19,84 @@ import epics		# PyEpics support
 import eznx		# NeXus r/w support using h5py
 
 
+SVN_ID = '$Id$'
 XML_CONFIGURATION_FILE = 'saveFlyData.xml'
 XSD_SCHEMA_FILE = 'saveFlyData.xsd'
 
-
-# TODO: refactor to identify parents and children
-dir_registry = {}
-class NX_structure(object):
-    
-    def __init__(self, parent_path, xml_element_node):
-        self.xml_node = xml_element_node
-        self.hdf5_group = None
-        self.name = xml_element_node.attrib['name']
-        self.nx_class = xml_element_node.attrib['class']
-        #node = xml_element_node.getparent()
-        if self.name != '/' and not parent_path.endswith('/'):
-            parent_path += '/'
-        self.hdf5_path = parent_path + self.name
+dir_registry = {}       # key: HDF5 absolute path, value: Dir_Specification object
+pv_registry = {}       # key: node/@label, value: PV_Specification object
 
 
-# TODO: refactor to identify parents
-pv_registry = {}
-class PV_specification(object):
+def getDirObjectByXmlNode(xml_node):
+    '''locate a Dir_Specification object by matching its xml_node'''
+    for dir_spec_obj in dir_registry.values():
+        if dir_spec_obj.xml_node == xml_node:
+            return dir_spec_obj
+    return None
+
+
+class Dir_Specification(object):
     
     def __init__(self, xml_element_node):
         self.hdf5_path = None
         self.xml_node = xml_element_node
+        self.hdf5_group = None
+        self.name = xml_element_node.attrib['name']
+        self.nx_class = xml_element_node.attrib['class']
+
+        xml_parent_node = xml_element_node.getparent()
+        self.dir_children = {}
+        if xml_parent_node.tag == 'dir':
+            # identify our parent
+            self.dir_parent = getDirObjectByXmlNode(xml_parent_node)
+            # next, find our HDF5 path from our parent
+            path = self.dir_parent.hdf5_path
+            if not path.endswith('/'):
+                path += '/'
+            self.hdf5_path = path + self.name
+            # finally, declare ourself to be a child of that parent
+            self.dir_parent.dir_children[self.hdf5_path] = self
+        elif xml_parent_node.tag == 'NX_structure':
+            self.dir_parent = None
+            self.hdf5_path = '/'
+        if self.hdf5_path in dir_registry:
+            msg = "Cannot create duplicate HDF5 path names: " + self.hdf5_path
+            raise RuntimeError, msg
+        dir_registry[self.hdf5_path] = self
+    
+    def __str__(self):
+        return self.hdf5_path or 'Dir_Specification object'
+
+
+class PV_Specification(object):
+    
+    def __init__(self, xml_element_node):
+        self.xml_node = xml_element_node
         self.label = xml_element_node.attrib['label']
+        if self.label in pv_registry:
+            msg = "Cannot use PV label more than once: " + self.label
+            raise RuntimeError, msg
         self.pvname = xml_element_node.attrib['pvname']
-        self.pv = epics.PV(self.pvname)
+        self.pv = None
         self.attrib = {}
+        self.length_limit = xml_element_node.get('length_limit', None)
         for node in xml_element_node.xpath('attribute'):
             self.attrib[node.attrib['name']] = node.attrib['value']
+        
+        # identify our parent
+        xml_parent_node = xml_element_node.getparent()
+        self.dir_parent = getDirObjectByXmlNode(xml_parent_node)
+        # finally, declare ourself to be a child of that parent
+        self.hdf5_path = self.dir_parent.hdf5_path + '/' + self.label
+        self.dir_parent.dir_children[self.hdf5_path] = self
+        pv_registry[self.label] = self
     
-    def parentDir(self):
-        # TODO: implement
-        raise NotImplementedError
+    def __str__(self):
+        try:
+            nm = self.label + ' <' + self.pvname + '>'
+        except:
+            nm = 'PV_Specification object'
+        return nm
 
 
 class SaveFlyScan(object):
@@ -69,16 +111,20 @@ class SaveFlyScan(object):
         path = self._get_support_code_dir()
         self.config_file = config_file or os.path.join(path, XML_CONFIGURATION_FILE)
         self._read_configuration()
-
-        self._createFile()
+        self._prepare_to_acquire()
 
     def waitForData(self):
         '''wait until the data is ready, then save it'''
-        trigger = epics.PV(self.trigger_pv)
-        # TODO: convert this polling loop to a PV monitor callback
-        while trigger.get() not in self.trigger_accepted_values:
+        self.completion_signal = False
+        self.trigger = epics.PV(self.trigger_pv, callback=self.triggerHandler)
+        while not self.completion_signal:
             time.sleep(self.trigger_poll_interval_s)
         self.saveFile()
+    
+    def triggerHandler(self, **keys):
+        '''receive the EPICS CA monitor on the trigger PV and save the data at the right time'''
+        if self.trigger.get() in self.trigger_accepted_values:
+            self.completion_signal = True
 
     def saveFile(self):
         '''write all desired data to the file and exit this code'''
@@ -86,17 +132,19 @@ class SaveFlyScan(object):
         timestamp = ' '.join((t.strftime("%Y-%m-%d"), t.strftime("%H:%M:%S")))
         eznx.addAttributes(self.structure['/'].hdf5_group, timestamp = timestamp)
 
-        for key, spec in self.pvdb.items():
-            #print key, spec
-            hdf5_parent = self._get_hdf5_parent_object(spec.xml_node)
-            value = spec.pv.get()
+        for pv_spec in self.pvdb.values():
+            value = pv_spec.pv.get()
             if not isinstance(value, numpy.ndarray):
                 value = [value]
-            #print pv.pvname, type(value)
-            #print key, pv, value, type(value)
-            ds = eznx.makeDataset(hdf5_parent, spec.label, value)
-            self._attachEpicsAttributes(ds, spec.pv)
-            eznx.addAttributes(ds, **spec.attrib)
+            else:
+                if pv_spec.length_limit and pv_spec.length_limit in pv_registry:
+                    length_limit = pv_registry[pv_spec.length_limit].pv.get()
+                    if len(value) > length_limit:
+                        value = value[:length_limit]
+            hdf5_parent = pv_spec.dir_parent.hdf5_group
+            ds = eznx.makeDataset(hdf5_parent, pv_spec.label, value)
+            self._attachEpicsAttributes(ds, pv_spec.pv)
+            eznx.addAttributes(ds, **pv_spec.attrib)
         
         self.structure['/'].hdf5_group.close()    # be CERTAIN to close the file
     
@@ -126,44 +174,25 @@ class SaveFlyScan(object):
         self.trigger_accepted_values = acceptable_values
         
         # TODO: figure how to pull default value from XML Schema
+        #       <xs:attribute name="poll_time_s" use="optional" type="xs:decimal" default="0.1"/>
+        # use an XPath search to get the node from xmlschema_doc
         default_value = self.trigger_poll_interval_s
         self.trigger_poll_interval_s = node.get('poll_time_s', default_value)
         
         nx_structure = root.xpath('/saveFlyData/NX_structure')[0]
-        self.structure = {}
         for node in nx_structure.xpath('//dir'):
-            parent = node.getparent()
-            if parent.tag == 'NX_structure':
-                hdf5_path = ''
-            else:
-                hdf5_path = self._get_parent_path(node)
-            xture = NX_structure(hdf5_path, node)
-            self.structure[xture.hdf5_path] = xture
+            Dir_Specification(node)
         
-        self.pvdb = {}
-        for node in root.xpath('//PV'):
-            spec = PV_specification(node)
-            spec.hdf5_path = self._get_parent_path(node)
-            self.pvdb[spec.label] = spec
-    
-    def _get_parent_path(self, xml_node):
-        parent = xml_node.getparent()
-        hdf5_path = None
-        for s in self.structure.values():
-            if s.xml_node == parent:
-                hdf5_path = s.hdf5_path
-                break
-        return hdf5_path
-    
-    def _get_hdf5_parent_object(self, xml_node):
-        return self.structure[self._get_parent_path(xml_node)].hdf5_group
+        for node in nx_structure.xpath('//PV'):
+            PV_Specification(node)
     
     def _get_support_code_dir(self):
         return os.path.split(os.path.abspath(__file__))[0]
   
-    def _createFile(self):
-        '''create the HDF5 file and structure'''
-        for key, xture in sorted(self.structure.items()):
+    def _prepare_to_acquire(self):
+        '''connect to EPICS and create the HDF5 file and structure'''
+        # create the file
+        for key, xture in sorted(dir_registry.items()):
             if key == '/':
                 # create the file and internal structure
                 f = eznx.makeFile(self.hdf5_file_name,
@@ -171,15 +200,20 @@ class SaveFlyScan(object):
                   file_name = self.hdf5_file_name,
                   instrument = "APS USAXS at 15ID-D",
                   scan_mode = 'USAXS fly scan',
-                  creator = '$Id$',
+                  creator = __file__,
                   creator_version = self.creator_version,
+                  svn_id = SVN_ID,
                   HDF5_Version = h5py.version.hdf5_version,
                   h5py_version = h5py.version.version,
                 )
                 xture.hdf5_group = f
             else:
-                hdf5_parent = self._get_hdf5_parent_object(xture.xml_node)
+                hdf5_parent = xture.dir_parent.hdf5_group
                 xture.hdf5_group = eznx.makeGroup(hdf5_parent, xture.name, xture.nx_class)
+
+        # connect to EPICS PVs
+        for pv_spec in pv_registry.values():
+            pv_spec.pv = epics.PV(pv_spec.pvname)
 
     def _attachEpicsAttributes(self, node, pv):
         '''attach common attributes from EPICS to the HDF5 tree node'''
@@ -217,11 +251,11 @@ def main():
 
 
 if __name__ == '__main__':
-    main()	# production system
+#     main()	# production system
 
     # code development
-#    sfs = SaveFlyScan('/tmp/test.h5', XML_CONFIGURATION_FILE)
-#    sfs.waitForData()
+    sfs = SaveFlyScan('test.h5', XML_CONFIGURATION_FILE)
+    sfs.waitForData()
 
 
 ########### SVN repository information ###################

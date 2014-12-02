87b19208f4bc658f0eb2d13490cc59212f3b46cf
Pete R Jemian <prjemian@users.noreply.github.com>
2014-12-02 00:52:51 +0000

    r/w the .rc file

--- qToolUsaxs/config.py
+++ qToolUsaxs/config.py
@@ -4,9 +4,12 @@ manage the configuration file
 '''
 
 
+import datetime
 import os
 import sys
 from lxml import etree
+
+CONFIG_FILE_VERSION = '2.0'
 
 
 class Position(object):
@@ -21,7 +24,7 @@ class Position(object):
 
 class ConfigFile(object):
     """
-    in-memory representation
+    in-memory representation of .rc file
     """
     
     def __init__(self, fname=None):
@@ -30,9 +33,9 @@ class ConfigFile(object):
         self.fname = fname
         self.param = {}
         self.positions = []
-        self.readfile()
+        self.rc_read()
     
-    def readfile(self):
+    def rc_read(self):
         root = etree.parse(self.fname)
         for node in root.xpath('//parameter'):
             param_name = node.attrib['name'].strip()
@@ -46,8 +49,40 @@ class ConfigFile(object):
             # ignore _row
             self.positions.append(position)
     
-    def writefile(self, fname=None):
-        pass
+    def rc_write(self, fname=None):
+        t = datetime.datetime.now()
+        yyyymmdd = t.strftime("%Y-%m-%d")
+        hhmmss = t.strftime("%H:%M:%S")
+
+        root = etree.ElementTree.Element("qTool")
+        root.set("version", CONFIG_FILE_VERSION)
+        root.set("date", yyyymmdd)
+        root.set("time", hhmmss)
+        root.append(etree.ElementTree.Comment("written by: " + 'qToolUsaxs.config.py'))
+        #root.append(ElementTree.ProcessingInstruction("example ProcessingInstruction()"))
+ 
+        ####################################
+        # add the items to the XML structure
+        ####################################
+
+        # user parameters
+        for key in sorted(self.param.keys()):
+            value = self.param[key]
+            node = etree.ElementTree.SubElement(root, "parameter")
+            node.set("name", key)
+            if len(value) > 0:
+                node.text = str(value)
+
+        # Q position table
+        for row, pos in enumerate(self.positions):
+            node = etree.ElementTree.SubElement(root, "position")
+            node.set("row",   str(row))
+            for key in "label Q".split():
+                value = pos.get(key)
+                if len(value) == 0: value = ''
+                node.set(key, value)
+        
+        # TODO: pretty print to the XML file: fname
 
 
 if __name__ == '__main__':
